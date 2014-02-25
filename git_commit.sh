e8ab5d577490dfc2a748ca1c7602a36566475f39
wanderanimrod <wanderanimrod@gmail.com>
2014-02-25 14:42:05 +0300

    #X [nim] Adding story number as input to gitcommit command and using x to mark missing arguments instead of more cumbersome empty quotes

--- git_commit.sh
+++ git_commit.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-PAIR=$1
-MESSAGE=$2
+STORY=$1
+PAIR=$2
+MESSAGE=$3
 
-if [[ -z "${MESSAGE}" ]]; then
-    echo -e "\nERROR: Cannot commit without a message!" 
-    echo -e "First argument must be your pair's alias. Pass '' as the first argument if you are working alone\n"
-    exit -1
+if [[ -z "${STORY}" ]]
+then
+    echo -e "\nERROR: Story number required! Use x as first argument if you are not working on a story\n"
+elif [ $STORY = "x" ]
+then
+    STORY="X"
 fi
 
 #Prefix pair name with comma if non-empty pair name is passed
-if ! [[ -z "${PAIR}" ]]; then
+if [[ -z "${PAIR}" ]]
+then
+    echo -e "\nERROR: Pair alias required! Use x as first argument if you are working alone\n"
+elif [ $PAIR = "x" ]
+then
+   PAIR=""
+else
     PAIR=", ${PAIR}"
 fi
 
-git commit -m "[nim${PAIR}] ${MESSAGE}"
+if [[ -z "${MESSAGE}" ]]
+then
+    echo -e "\nERROR: Cannot commit without a message! Third argument must be your commit message.\n"
+    exit -1
+fi
+
+git commit -m "#${STORY} [nim${PAIR}] ${MESSAGE}"
